53e6c1b8dbe0636fc82360494dc078e3701c4920
Ryan Layer <ryan.layer@gmail.com>
2023-09-07 09:23:20 -0600

    Update print_cases.py

--- print_cases.py
+++ print_cases.py
@@ -1,6 +1,6 @@
-county='Boulder'
+country='United States of America'
 county_column = 1
-cases_column = 4
-file_name = 'covid-19-data/us-counties.csv'
-cases = get_column()
-print(cases)
+fires_column = 4
+file_name = 'Agrofood_co2_emission.csv'
+fires = get_column()
+print(fires)
