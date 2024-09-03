c464f0ce811efc007d77c1a98e0ccd0fb067cb5c
Erik Johnson <airik.k.johnson@gmail.com>
2024-09-03 16:48:51 -0600

    update

--- print_fires.py
+++ print_fires.py
@@ -1,6 +1,7 @@
-country='United States of America'
-county_column = 1
-fires_column = 4
-file_name = 'Agrofood_co2_emission.csv'
-fires = get_column()
-print(fires)
+country = "United States of America"
+country_column_index = -1  # TODO: update this
+file_path = "./Agrofood_co2_emission.csv"
+# TODO
+# add code to compute the number of fires so that the following print statement prints
+# the number of fires in the given country
+print(f"There were {num_fires} fires in {country}")
