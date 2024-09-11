90f7754fa955230874fd58fa97bac6ead0b83a4e
samslatcher <44731395+samslatcher@users.noreply.github.com>
2024-09-10 22:06:06 -0600

    created environment and built new function

---
+++ utils.py
@@ -0,0 +1,11 @@
+def get_rows_by_column_value(file_path, column_index, column_num):
+   matching_rows = []
+   f = open(file_path, "r")
+   for x in f:
+        txt = f.readline()
+        i_list = txt.split(", ")
+        column_value = len(i_list)
+        if column_index == column_value:
+            matching_rows.append(i_list)
+            print(matching_rows)
+    return matching_rows
