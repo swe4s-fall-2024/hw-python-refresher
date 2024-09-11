1010396dfb3e91c092fd6c4b3ed494190ec6e09a
samslatcher <44731395+samslatcher@users.noreply.github.com>
2024-09-10 22:39:14 -0600

    changelog added

--- utils.py
+++ utils.py
@@ -1,10 +1,9 @@
-def get_rows_by_column_value(file_path, column_index):
+def get_rows_by_column_value(file_path, column_index=0, column_value=0):
    matching_rows = []
    f = open(file_path, "r")
    for x in f:
         txt = f.readline()
         i_list = txt.split(", ")
-        column_value = len(i_list)
         if column_index == column_value:
             matching_rows.append(i_list)
             print(matching_rows)
